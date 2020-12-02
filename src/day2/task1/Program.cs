d08a5ea984c9676649ef1e80fb4d0930c5b42f72
Antonín Procházka <antonin@postsharp.net>
2020-12-02 10:31:21 +0100

    Day2.Task1 done.

---
+++ src/day2/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+
+var regex = new Regex(@"^(?<min>\d+)-(?<max>\d+) (?<character>[a-zA-Z]): (?<password>[a-zA-Z]+)$");
+
+int validCount = 0;
+
+foreach (string line in lines)
+{
+    var match = regex.Match(line);
+    int min = int.Parse(match.Groups["min"].Value);
+    int max = int.Parse(match.Groups["max"].Value);
+    char character = match.Groups["character"].Value[0];
+    string password = match.Groups["password"].Value;
+
+    int sum = password.Sum(c => c == character ? 1 : 0);
+
+    if (sum >= min && sum <= max)
+        validCount++;
+}
+
+WriteLine(validCount);
