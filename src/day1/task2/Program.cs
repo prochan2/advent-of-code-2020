d412aa48235522c0934cbc4a24380b1c1befd399
Antonín Procházka <antonin@postsharp.net>
2020-12-02 09:40:35 +0100

    Day1.Task2 done.

---
+++ src/day1/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+var ints = lines.Select(line => int.Parse(line)).ToArray();
+
+for (int i = 0; i < ints.Length; i++)
+{
+    for (int j = i + 1; j < ints.Length; j++)
+    {
+        for (int k = j + 1; k < ints.Length; k++)
+        {
+            if (ints[i] + ints[j] + ints[k] == 2020)
+            {
+                WriteLine(ints[i] * ints[j] * ints[k]);
+            }
+        }
+    }
+}
