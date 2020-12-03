98ce8ca17a7cd51596cd839626134d1a724b5cf3
Antonín Procházka <antonin@postsharp.net>
2020-12-03 06:46:06 +0100

    Day 3 done.

---
+++ src/day3/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+
+int treesCount = 0;
+int x = 0;
+
+foreach (string line in lines.Skip(1))
+{
+    x += 3;
+
+    if (x >= line.Length)
+        x -= line.Length;
+
+    if (line[x] == '#')
+        treesCount++;
+}
+
+WriteLine(treesCount);
---
+++ src/day3/task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt").Where(line => !string.IsNullOrEmpty(line)).ToArray();
+
+long product = 1;
+
+product *= Count(1, 1);
+product *= Count(3, 1);
+product *= Count(5, 1);
+product *= Count(7, 1);
+product *= Count(1, 2);
+
+WriteLine(product);
+
+long Count(int xStep, int yStep)
+{
+    long treesCount = 0;
+    int x = 0;
+    int y = 0;
+
+    do
+    {
+        y += yStep;
+
+        if (y >= lines.Length)
+            y = lines.Length - 1;
+
+        x += xStep;
+
+        if (x >= lines[y].Length)
+            x -= lines[y].Length;
+
+        if (lines[y][x] == '#')
+            treesCount++;
+    }
+    while (y != lines.Length - 1);
+
+    WriteLine(treesCount);
+
+    return treesCount;
+}
