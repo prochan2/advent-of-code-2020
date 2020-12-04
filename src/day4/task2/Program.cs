b5570d627cbda2f1e09eda37f4d61b435cca74ca
Antonín Procházka <antonin@postsharp.net>
2020-12-04 07:08:59 +0100

    Day4.Task2 cleared and substring replaced by ranges.

--- src/day4/task2/Program.cs
+++ src/day4/task2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 using static System.Console;
 
@@ -10,123 +9,116 @@ var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
 long items = 0;
 long valid = 0;
 
-foreach (string line in lines)
+for (int lineNumber = 0; lineNumber < lines.Length; lineNumber++)
 {
-    if (line == "")
+    try
     {
-        WriteLine();
+        var pairs = lines[lineNumber].Split(' ');
 
-        if (items == 7)
+        Regex hclRegex = new Regex("^#[0-9a-f]{6}$");
+        Regex pidRegex = new Regex("^[0-9]{9}$");
+
+        var ecls = new HashSet<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+
+        foreach (string pair in pairs)
         {
-            valid++;
-            WriteLine("valid");
-        }
-        else
-        {
-            WriteLine("invalid");
-        }
+            Write(pair);
+            Write(" ");
 
-        items = 0;
+            var kv = pair.Split(':', 2);
+            bool isNumber = long.TryParse(kv[1], out long value);
 
-        continue;
-    }
-
-    var pairs = line.Split(' ');
-
-    Regex hclRegex = new Regex("^#[0-9a-f]{6}$");
-    Regex pidRegex = new Regex("^[0-9]{9}$");
-
-    var ecls = new HashSet<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-
-    foreach (string pair in pairs)
-    {
-        Write(pair);
-        Write(" ");
-
-        var kv = pair.Split(':', 2);
-        bool isNumber = long.TryParse(kv[1], out long value);
-
-        switch (kv[0])
-        {
-            case "byr":
-                if (!isNumber || value < 1920 || value > 2002)
-                    continue;
-                break;
-
-            case "iyr":
-                if (!isNumber || value < 2010 || value > 2020)
-                    continue;
-                break;
-
-            case "eyr":
-                if (!isNumber || value < 2020 || value > 2030)
-                    continue;
-                break;
-
-            case "hgt":
-                string x = kv[1].Substring(0, kv[1].Length - 2);
-                if (!long.TryParse(x, out value))
-                    continue;
-
-                string unit = kv[1].Substring(kv[1].Length - 2);
-
-                switch (unit)
-                {
-                    case "cm":
-                        if (value < 150 || value > 193)
-                            continue;
-                        break;
-
-                    case "in":
-                        if (value < 59 || value > 76)
-                            continue;
-                        break;
-
-                    default:
+            switch (kv[0])
+            {
+                case "byr":
+                    if (!isNumber || value < 1920 || value > 2002)
                         continue;
-                }
+                    break;
 
-                break;
+                case "iyr":
+                    if (!isNumber || value < 2010 || value > 2020)
+                        continue;
+                    break;
 
-            case "hcl":
-                if (!hclRegex.IsMatch(kv[1]))
+                case "eyr":
+                    if (!isNumber || value < 2020 || value > 2030)
+                        continue;
+                    break;
+
+                case "hgt":
+                    string x = kv[1][0..^2];
+                    if (!long.TryParse(x, out value))
+                        continue;
+
+                    string unit = kv[1][^2..];
+
+                    switch (unit)
+                    {
+                        case "cm":
+                            if (value < 150 || value > 193)
+                                continue;
+                            break;
+
+                        case "in":
+                            if (value < 59 || value > 76)
+                                continue;
+                            break;
+
+                        default:
+                            continue;
+                    }
+
+                    break;
+
+                case "hcl":
+                    if (!hclRegex.IsMatch(kv[1]))
+                        continue;
+                    break;
+
+                case "ecl":
+                    if (!ecls.Contains(kv[1]))
+                        continue;
+                    break;
+
+                case "pid":
+                    if (!pidRegex.IsMatch(kv[1]))
+                        continue;
+                    break;
+
+                case "cid":
                     continue;
-                break;
 
-            case "ecl":
-                if (!ecls.Contains(kv[1]))
-                    continue;
-                break;
+                default:
+                    throw new NotImplementedException();
+            }
 
-            case "pid":
-                if (!pidRegex.IsMatch(kv[1]))
-                    continue;
-                break;
+            Write("OK");
+            Write(" ");
 
-            case "cid":
-                continue;
-
-            default:
-                throw new NotImplementedException();
+            items++;
         }
-
-        Write("OK");
-        Write(" ");
-
-        items++;
     }
-}
+    finally
+    {
+        if (lineNumber == lines.Length - 1 || lines[lineNumber + 1] == "")
+        {
+            lineNumber++;
 
-WriteLine();
+            WriteLine();
 
-if (items == 7)
-{
-    valid++;
-    WriteLine("valid");
-}
-else
-{
-    WriteLine("invalid");
+            if (items == 7)
+            {
+                valid++;
+                WriteLine("valid");
+            }
+            else
+            {
+                WriteLine("invalid");
+            }
+
+            items = 0;
+        }
+    }
 }
 
 WriteLine(valid);
