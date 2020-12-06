459d6fedf6b15b2760fc8ff9f6c587e20a58aa1d
Antonín Procházka <antonin@postsharp.net>
2020-12-06 06:22:55 +0100

    Day 6 done.

---
+++ src/day6/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+
+HashSet<char> group = new HashSet<char>();
+int sum = 0;
+
+foreach (var line in lines)
+{
+    if (line == "")
+    {
+        sum += group.Count;
+        group.Clear();
+    }
+
+    foreach (char c in line)
+        group.Add(c);
+}
+
+sum += group.Count;
+
+WriteLine(sum);
---
+++ src/day6/task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+var lines = File.ReadAllLines(@"..\..\..\..\input.txt");
+
+HashSet<char> group = new HashSet<char>();
+int sum = 0;
+bool @new = true;
+
+foreach (var line in lines)
+{
+    WriteLine(line);
+
+    if (line == "")
+    {
+        WriteLine(group.Count);
+
+        sum += group.Count;
+        group.Clear();
+        @new = true;
+        continue;
+    }
+
+    HashSet<char> person = new HashSet<char>();
+
+    foreach (char c in line)
+        person.Add(c);
+
+    if (@new)
+    {
+        group = person;
+        @new = false;
+    }
+    else
+    {
+        group.IntersectWith(person);
+    }
+}
+
+sum += group.Count;
+
+WriteLine(sum);
