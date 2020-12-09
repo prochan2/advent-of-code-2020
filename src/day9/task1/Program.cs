e9041d054efb597ee5e373ea59b3dcfed91c7b81
Antonín Procházka <antonin@postsharp.net>
2020-12-09 06:49:26 +0100

    Day 9 done.

---
+++ src/day9/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+//string fileName = @"..\..\..\..\sinput.txt";
+//var preambleLength = 5;
+
+string fileName = @"..\..\..\..\input.txt";
+var preambleLength = 25;
+
+var lines = File.ReadAllLines(fileName);
+var numbers = lines.Select(l => long.Parse(l)).ToArray();
+
+long answer = long.MinValue;
+
+for (int i = preambleLength; i < numbers.Length; i++)
+{
+    if (!HasSum(i))
+    {
+        answer = numbers[i];
+        break;
+    }
+}
+
+WriteLine(answer);
+
+bool HasSum(int i)
+{
+    var n = numbers[i];
+
+    for (int j = 0; j < preambleLength; j++)
+    {
+        for (int k = 0; k < preambleLength; k++)
+        {
+            if (j == k)
+                continue;
+
+            if (numbers[i - j - 1] + numbers[i - k - 1] == n)
+                return true;
+        }
+    }
+
+    return false;
+}
---
+++ src/day9/task2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+//string fileName = @"..\..\..\..\sinput.txt";
+//var preambleLength = 5;
+
+string fileName = @"..\..\..\..\input.txt";
+var preambleLength = 25;
+
+var lines = File.ReadAllLines(fileName);
+var numbers = lines.Select(l => long.Parse(l)).ToArray();
+
+long answer1 = long.MinValue;
+
+for (int x = preambleLength; x < numbers.Length; x++)
+{
+    if (!HasSum(x))
+    {
+        answer1 = numbers[x];
+        break;
+    }
+}
+
+int i;
+int j = -1;
+
+for (i = 0; i < numbers.Length; i++)
+{
+    var n1 = numbers[i];
+    long sum = n1;
+    bool found = false;
+
+    for (j = i + 1; j < numbers.Length; j++)
+    {
+        var n2 = numbers[j];
+        sum += n2;
+
+        if (sum > answer1)
+            break;
+
+        if (sum == answer1)
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+        break;
+}
+
+var sortedSequence = numbers[i..(j + 1)];
+Array.Sort(sortedSequence);
+
+var answer2 = sortedSequence[0];
+var answer3 = sortedSequence[^1];
+var answer4 = answer2 + answer3;
+
+WriteLine(answer2);
+WriteLine(answer3);
+WriteLine(answer4);
+
+bool HasSum(int i)
+{
+    var n = numbers[i];
+
+    for (int j = 0; j < preambleLength; j++)
+    {
+        for (int k = 0; k < preambleLength; k++)
+        {
+            if (j == k)
+                continue;
+
+            if (numbers[i - j - 1] + numbers[i - k - 1] == n)
+                return true;
+        }
+    }
+
+    return false;
+}
