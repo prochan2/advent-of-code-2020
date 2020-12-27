680bdb73dab55c4420e3838a2bbd733c1bb27377
Antonín Procházka <antonin@postsharp.net>
2020-12-27 21:24:42 +0100

    Day11.Task1 done.

--- src/day11/task1/Program.cs
+++ src/day11/task1/Program.cs
@@ -5,10 +5,8 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using static System.Console;
 
-string fileName = @"..\..\..\..\sinput.txt";
-//string fileName = @"..\..\..\..\input.txt";
-
-const int adjacencyLength = 8;
+//string fileName = @"..\..\..\..\sinput.txt";
+string fileName = @"..\..\..\..\input.txt";
 
 var seats = File.ReadAllLines(fileName).Select(l => l.Select(c => c switch
 {
@@ -21,148 +19,61 @@ var seats = File.ReadAllLines(fileName).Select(l => l.Select(c => c switch
 int xLength = seats.Length;
 int yLength = seats[0].Length;
 
-var seatCounts = seats.Select(l => l.Select(s => 0).ToArray()).ToArray();
-
-//WriteArray(seats);
-
-TryMakeSeats();
-
-WriteSeats(seats);
-
-while (true)
+while (TryMakeSeats())
 {
-    CountAll();
-    bool done = !TryMakeSeats();
-
-    WriteArray(seatCounts);
-    WriteSeats(seats);
-    ReadLine();
 }
 
-//WriteArray(seatCounts);
-
-WriteLine(seats.Sum(s => s.Count(s => s == 1)));
-
-void CountLine(int x, int y, int dx, int dy, bool reverse = false)
-{
-    int step = 1;
-    int sum = 0;
-
-    for (; x >= 0 && x < xLength && y >= 0 && y < yLength; x += dx, y += dy)
-    {
-        if (seats[x][y] == -1)
-            continue;
-
-        seatCounts[x][y] += sum;
-
-        //WriteLine(sum);
-        //WriteArray(seatCounts);
-        //ReadLine();
-
-        sum += seats[x][y];
-
-        if (step++ <= adjacencyLength)
-            continue;
-
-        sum -= seats[x - (adjacencyLength * dx)][y - (adjacencyLength * dy)];
-    }
-
-    if (reverse)
-        return;
-
-    CountLine(x - dx, y - dy, -dx, -dy, true);
-}
-
-void CountAll()
-{
-    for (int x = 0; x < xLength; x++)
-    {
-        for (int y = 0; y < yLength; y++)
-        {
-            seatCounts[x][y] = 0;
-        }
-    }
-
-    for (int x = 0; x < xLength; x++)
-    {
-        CountLine(x, 0, 0, 1);
-    }
-
-    for (int y = 0; y < yLength; y++)
-    {
-        CountLine(0, y, 1, 0);
-    }
-
-    for (int x = 0; x < xLength; x++)
-    {
-        CountLine(x, 0, 1, 1);
-        CountLine(x, 0, -1, 1);
-    }
-
-    for (int y = 1; y < yLength; y++)
-    {
-        CountLine(0, y, 1, 1);
-        CountLine(0, y, -1, 1);
-    }
-}
+WriteLine(seats.Sum(row => row.Count(seat => seat == 1)));
 
 bool TryMakeSeats()
 {
     bool changed = false;
+    var newSeats = seats.Select(row => row.Select(seat => seat).ToArray()).ToArray();
 
     for (int x = 0; x < xLength; x++)
     {
         for (int y = 0; y < yLength; y++)
         {
-            switch ((seats[x][y], seatCounts[x][y]))
+            if (seats[x][y] == -1)
+                continue;
+
+            int adjacentCount = CountAdjacent(x, y);
+
+            if (seats[x][y] == 0 && adjacentCount == 0)
             {
-                case (0, 0):
-                    seats[x][y] = 1;
-                    changed = true;
-                    break;
-                case (1, >= 4):
-                    seats[x][y] = 0;
-                    changed = true;
-                    break;
+                newSeats[x][y] = 1;
+                changed = true;
+            }
+            else if (seats[x][y] == 1 && adjacentCount >= 4)
+            {
+                newSeats[x][y] = 0;
+                changed = true;
             }
         }
     }
 
+    seats = newSeats;
     return changed;
 }
 
-void WriteArray(int[][] array)
+int CountAdjacent(int x, int y)
 {
-    for (int x = 0; x < xLength; x++)
-    {
-        for (int y = 0; y < yLength; y++)
-        {
-            Write(array[x][y]);
-            Write(" ");
-        }
-        WriteLine();
-    }
-
-    WriteLine();
+    int adjacentCount = 0;
+    adjacentCount += IsOcupied(x - 1, y - 1);
+    adjacentCount += IsOcupied(x + 1, y - 1);
+    adjacentCount += IsOcupied(x - 1, y + 1);
+    adjacentCount += IsOcupied(x + 1, y + 1);
+    adjacentCount += IsOcupied(x, y - 1);
+    adjacentCount += IsOcupied(x, y + 1);
+    adjacentCount += IsOcupied(x - 1, y);
+    adjacentCount += IsOcupied(x + 1, y);
+    return adjacentCount;
 }
 
-void WriteSeats(int[][] array)
+int IsOcupied(int x, int y)
 {
-    for (int x = 0; x < xLength; x++)
-    {
-        for (int y = 0; y < yLength; y++)
-        {
-            Write(array[x][y] switch
-            {
-                0 => 'L',
-                1 => '#',
-                -1 => '.',
-                _ => throw new InvalidOperationException()
-            });
-            Write(" ");
-        }
-        WriteLine();
-    }
+    if (x < 0 || x >= xLength || y < 0 || y >= yLength)
+        return 0;
 
-    WriteLine();
+    return seats[x][y] == 1 ? 1 : 0;
 }
